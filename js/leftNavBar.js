5f46a98df04ae286a72fda24f4d8e4d87b72f20e
Ximena Hasbach <xhasbach@dhcp-18-111-11-73.dyn.MIT.EDU>
2015-12-06 14:35:57 -0500

    bar edits

--- js/leftNavBar.js
+++ js/leftNavBar.js
@@ -1,20 +1,43 @@
-var bodyText = 
-	'<form class="filterform" action="#">
-        <input class="filterinput" type="text">
-    </form>';
+$(document).ready(function(){
+	var bodyText = '<form class="filterform" action="#"><input class="filterinput" type="text"></form>';
 
-var items = ["australia", "austria", "belgium"]
+	var items = ["australia", "austria", "belgium"]
 
-function createDemoList(names){
-	listHTML = ""
-	listHTML +=  '<ul id="list">';
-	for (var i in names){
-		listHTML += '<li><a href="#/' + names[i] + '/">' + names[i] + '</a></li>'
+	function createDemoList(names){
+		listHTML = ""
+		listHTML +=  '<ul id="list">';
+		for (var i in names){
+			listHTML += '<li><a href="#/' + names[i] + '/">' + names[i] + '</a></li>'
+		}
+		listHTML += '</ul>';
+		return listHTML;
+
 	}
-	listHTML += '</ul>';
+	var items = ["australia", "austria", "belgium"]
 
-}
-var items = ["australia", "austria", "belgium"]
+	var itemsText = createDemoList(items)
+	bodyText += itemsText;
+	$("#left_nav_bar").html(bodyText);
 
-var itemsText = createDemoList(items)
-bodyText += itemsText;
+	(function($) {
+
+    $('.filterinput').keyup(function() {
+        var a = $(this).val();
+        if (a.length > 2) {
+            // this finds all links in the list that contain the input,
+            // and hide the ones not containing the input while showing the ones that do
+            var containing = $('#list li').filter(function () {
+                var regex = new RegExp('\\b' + a, 'i');
+                return regex.test($('a', this).text());
+            }).slideDown();
+            $('#list li').not(containing).slideUp();
+        } else {
+            $('#list li').slideDown();
+        }
+        return false;
+    })
+
+}(jQuery));
+})
+
+
